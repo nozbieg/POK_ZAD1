8fd4d437c116cf98ad1e26956e297bfeaa5cc6a2
Norbert Olesiak-Zbieg <nolesiak@gabos.pl>
2021-05-20 13:48:32 +0200

    Add project files.

---
+++ CubeVolume.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POK_ZAD1
+{
+    public class CubeVolume
+    {
+        public CubeVolume(double side)
+        {
+            Side = side;
+        }
+        public double Side { get; set; }
+
+        private double volume;
+        public double Volume
+        {
+            get
+            {
+                volume = Math.Pow(Side, 3);
+                return volume;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Cube Side is: " + Side + ", Cube Volume is: " + Volume;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace POK_ZAD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cube1 = new CubeVolume(5);
+            Console.WriteLine("Cube 1");
+            Console.WriteLine(cube1.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
